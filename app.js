5e3b69edfd70f69754e2bb18a8b50741ca6edb45
Slayer <aksuplayz1337@gmail.com>
2021-01-29 17:43:47 +0200

    Update logger

--- app.js
+++ app.js
@@ -4,36 +4,37 @@ const app = express();
 const DBL = require('@top-gg/sdk');
 const mongoose = require('mongoose');
 const User = require('./database/schemas/User');
-const webhook = new DBL.Webhook(process.env.DBL_STRING); // random string because yes
+const webhook = new DBL.Webhook(process.env.DBL_STRING);
+const logger = require('./logger');
 
-const port = process.env.PORT || process.env.port - 1 + 2 // available ports for Slayer's Raspberry
+const port = process.env.PORT || process.env.port - 1 + 2
 
 if (!process.env.MONGODB_URI) throw new Error('MONGODB_URI environment variable required');
 if (!process.env.DBL_STRING) throw new Error('DBL_STRING environment variable required');
 if (!process.env.PORT) throw new Error('PORT environment variable required. How can you run the app without a port? Wake up');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(err => {
-    throw new Error(err)
-});
 
-mongoose.connection.on('connected', () => console.log('[INFO] Mongoose connected'))
+mongoose.connection.on('connected', () => logger.log('Mongoose connected', { color: 'green', tags: ['INFO', 'DATABASE'] }))
 
-app.post('/dblwebhook', webhook.middleware(), async (req, res) => {
-    let amount = req.vote.isWeekend ? 2000 : 1000
+app.post('/dblwebhook', webhook.middleware(), async (req) => {
+    let credits = req.vote.isWeekend ? 2000 : 1000;
 
-    if (req.vote.type === 'test') console.log(req.vote);
+    logger.log(`User with id "${req.vote.user}" just voted ${process.env.BOT_NAME || 'your bot'}! Rewarded the user with $${credits}!`, { color: 'green', tags: ['DBL', 'DATABASE'] })
 
     const userSettings = await User.findOne({ discordId: req.vote.user });
+    if (!userSettings) return User.create({ discordId: req.vote.user, wallet: credits, votes: 1, lastVoted: Date.now() });
 
-    console.log(`User with id "${req.vote.user}" just voted ${process.env.BOT_NAME || 'your bot'}! Rewarded the user with $${amount}!`)
-
-    if (!userSettings) return User.create({ discordId: req.vote.user, wallet: amount, votes: 1, lastVoted: Date.now() });
-    await userSettings.updateOne({ wallet: userSettings.wallet + amount || amount, votes: userSettings.votes + 1 || 1, lastVoted: Date.now() })
+    await userSettings.updateOne({ wallet: userSettings.wallet + credits || credits,  votes: userSettings.votes + 1 || 1, lastVoted: Date.now()});
 });
 
-app.listen(port);
-console.log(`[INFO] Webserver running on ${port}`);
+app.listen(port, () => {
+    logger.log(`Webserver running on ${port}`, { color: 'green', tags: ['INFO', 'SERVER'] });
+
+    mongoose.connect(process.env.MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).catch(err => {
+        throw new Error(err)
+    });
+});
