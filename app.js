0a02f754f8d98c464dac28faaa1816479add182d
Slayer <aksuplayz1337@gmail.com>
2021-01-29 18:51:04 +0200

    Get the username who voted

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const mongoose = require('mongoose');
 const User = require('./database/schemas/User');
 const webhook = new DBL.Webhook(process.env.DBL_STRING);
 const logger = require('./logger');
+const fetch = require('node-fetch');
 
 const port = process.env.PORT || process.env.port - 1 + 2
 
 if (!process.env.MONGODB_URI) throw new Error('MONGODB_URI environment variable required');
 if (!process.env.DBL_STRING) throw new Error('DBL_STRING environment variable required');
+if (!process.env.TOKEN) throw new Error('TOKEN environment variable required');
 if (!process.env.PORT) throw new Error('PORT environment variable required. How can you run the app without a port? Wake up');
 
 
@@ -19,7 +21,13 @@ mongoose.connection.on('connected', () => logger.log('Mongoose connected', { col
 app.post('/dblwebhook', webhook.middleware(), async (req) => {
     let credits = req.vote.isWeekend ? 2000 : 1000;
 
-    logger.log(`User with id "${req.vote.user}" just voted ${process.env.BOT_NAME || 'your bot'}! Rewarded the user with $${credits}!`, { color: 'green', tags: ['DBL', 'DATABASE'] })
+    const apiUser = await fetch(`https://discord.com/api/v8/users/${req.vote.user}`, {
+        headers: {
+          Authorization: `Bot ${process.env.TOKEN}`
+        }
+    }).then(res => res.json());
+
+    logger.log(`${apiUser.username} (${req.vote.user}) just voted ${process.env.BOT_NAME || 'your bot'}! Rewarded the user with $${credits}!`, { color: 'green', tags: ['DBL', 'DATABASE'] })
 
     const userSettings = await User.findOne({ discordId: req.vote.user });
     if (!userSettings) return User.create({ discordId: req.vote.user, wallet: credits, votes: 1, lastVoted: Date.now() });
