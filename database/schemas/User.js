477ada9954aaf61ea2abce83c4e16801a78540e9
Slayer <aksuplayz1337@gmail.com>
2021-03-07 18:50:30 +0200

    Wallet from String to Number

--- database/schemas/User.js
+++ database/schemas/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     discordId: { type: String, required: true },
-    wallet: { type: String },
+    wallet: { type: Number },
     lastVoted: { type: Number },
     votes: { type: Number }
 });
